ba428e25b65457f4917e3d2becf45262e9c5be36
AJIyanu <aderemijoseph@live.com>
2023-01-05 23:21:41 +0100

    learninf never emds

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 """assign me safe annotation"""
+
+
+from typing import List, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:x:
+    """imoortant comment herr"""
+    zoomed_in: Tuple = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
