db519320cfb25b7a71f222285298067cc908c7f6
AJIyanu <aderemijoseph@live.com>
2023-01-05 23:16:40 +0100

    not return

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""assign me safe annotation"""
+
+
+from typing import Union, Sequence, Any
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """necessary is commentato"""
+    if lst:
+        return lst[0]
+    else:
+        return None
---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+"""assign me safe annotation"""
