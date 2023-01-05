5cf04b33fb4c62490700ece59532bfbe930f9990
AJIyanu <aderemijoseph@live.com>
2023-01-05 23:27:51 +0100

    igbagbe ko ni ko ba mi

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -7,7 +7,7 @@ from typing import List, Tuple
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """imoortant comment herr"""
-    zoomed_in: Tuple = [
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
