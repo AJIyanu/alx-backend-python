97069a4cc36bcebdfb8e05afcae6933cd4149b87
AJIyanu <aderemijoseph@live.com>
2023-01-06 00:01:19 +0100

    worked a little"

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 """assign me safe annotation"""
+
+
+from typing import Any, Mapping, Union, TypeVar
+
+
+T = TypeVar("T")
+def safely_get_value(dct: Mapping,
+                     key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    """This must be present"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
