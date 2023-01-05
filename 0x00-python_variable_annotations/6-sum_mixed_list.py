a3211cebea2064127cb488a100515bccda09505d
AJIyanu <aderemijoseph@live.com>
2023-01-05 20:07:07 +0100

    mix me

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """takes list and return one float"""
     lsum: float = 0
     for i in range(len(mxd_lst)):
