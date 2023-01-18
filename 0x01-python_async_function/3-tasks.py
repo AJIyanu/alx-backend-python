365ebac76fff23813a7ea72adeaa5665d59d7152
AJIyanu <aderemijoseph@live.com>
2023-01-18 21:47:11 +0100

    I just returned task sah

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""create and return async task"""
+
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> any:
+    """return an asyncio task"""
+    return asyncio.Task(wait_random(max_delay))
