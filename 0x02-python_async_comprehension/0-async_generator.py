34b7460bd3a31a97cb872e9cfd4c8fa3a0d60999
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-10-11 10:56:29 +0100

    correct typing

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,11 +2,11 @@
 """generator to yield random number"""
 
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 from random import randrange, random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """generate random shits"""
     for i in range(10):
         yield random() + randrange(0, 10)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,9 +2,10 @@
 """creating async comprehension from generator"""
 
 
+from  typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list:
+async def async_comprehension() -> List[float]:
     """collects list from generator and creates a new"""
     return [i async for i in async_generator()]
