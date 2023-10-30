16fc87b64f4fd474c503638c9e0a96f56b86b7af
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-10-30 15:32:00 +0100

    testing nested map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,6 +9,11 @@ import parameterized
 class TestAccessNestedMap(unittest.TestCase):
     """unit test for utils.access_nested_map"""
 
-    @parameterized.expand
-    def test_access_nested_map(self):
+    @parameterized.expand([
+                            ({"a": 1}, ("a",), 1),
+                            ({"a": {"b": 2}}, ("a",), {'b': 2}),
+                            ({"a": {"b": 2}}, ("a", "b"), 2)
+        ])
+    def test_access_nested_map(self, nested_map, path, result):
         """tests that a nested map returns right value"""
+        self.assertEqual(access_nested_map(nested_map=nested_map, path=path), result)
