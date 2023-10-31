8a462fa26b3b09cba75872c08b0ea1f5ac1517b3
AJ Iyanu <aderemijoseph@live.com>
2023-10-31 16:07:10 +0100

    sync

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 
 from utils import access_nested_map, get_json, memoize
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from parameterized import parameterized
 from typing import Mapping, Sequence, Any, Dict
 
@@ -63,7 +63,7 @@ class TestMemoize(unittest.TestCase):
     """ tests the memoize decorstor momorize """
 
     def test_memoize(self) -> None:
-        """ calls a function twice but method cslked once """
+        """ calls a function twice but method called once """
         class TestClass:
             
             def a_method(self):
@@ -73,7 +73,8 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, "a_method", return_value=42) as check:
+        with patch.object(TestClass, "a_method",
+                return_value=MagicMock(return_value=42)) as check:
             test = TestClass()
             self.assertEqual(test.a_property(), 42)
             self.assertEqual(test.a_property(), 42)
