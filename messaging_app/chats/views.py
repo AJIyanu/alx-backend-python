80a5470f5227aff1da3ab4240886d25a21abfe67
AJ Iyanu <aderemijoseph@live.com>
2025-07-20 22:34:11 +0100

    implement Conversation and Message viewsets with user-specific querysets

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer, UserSerializer
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing and editing conversation instances.
+    Provides 'list', 'create', 'retrieve', 'update', 'partial_update', and 'destroy' actions.
+    """
+    queryset = Conversation.objects.all().order_by('-created_at')
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:
+            return Conversation.objects.filter(participants=user).order_by('-created_at')
+        return Conversation.objects.none()
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing and editing message instances.
+    Provides 'list', 'create', 'retrieve', 'update', 'partial_update', and 'destroy' actions.
+    """
+    queryset = Message.objects.all().order_by('sent_at')
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:
+            user_conversations = Conversation.objects.filter(participants=user)
+            return Message.objects.filter(conversation__in=user_conversations).order_by('sent_at')
+        return Message.objects.none()
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
