ae20332b5a8b64c3a78a523acd65deb4eca2169b
AJ Iyanu <aderemijoseph@live.com>
2025-08-01 13:26:31 +0100

    fix: correct import statement for routers and clean up URL registration

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,16 +1,16 @@
 # chat/urls.py
 
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework_nested import routers
 from .views import ConversationViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='message')
 
 convo_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
 convo_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
-router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
     path('', include(router.urls)),
