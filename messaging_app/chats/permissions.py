c15324e6aa2c2ce467cda58e8d06a930caa9e0bb
AJ Iyanu <aderemijoseph@live.com>
2025-08-02 08:14:13 +0100

    fix: update import statements for permission classes to use the correct module

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,13 +1,13 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class IsParticipant(BasePermission):
+class IsParticipant(permissions.BasePermission):
     """
     Allows access only to participants of a conversation.
     """
     def has_object_permission(self, request, view, obj):
         return request.user in obj.participants.all()
 
-class IsMessageSenderOrParticipant(BasePermission):
+class IsMessageSenderOrParticipant(permissions.BasePermission):
     """
     Allows access only to the sender or conversation participant.
     """
